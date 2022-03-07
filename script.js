adb44baaab90165efe9a7382523d69d9c8df1db3
Fabricio Nascimento <fabricionascimentosiqueira@hotmail.com>
2022-03-07 16:22:33 -0300

    up slider

---
+++ script.js
@@ -0,0 +1,26 @@
+let slider = document.querySelector(".slider-largura");
+let img = document.querySelectorAll(".slide-img");
+slider.style.width = `calc(100vw * ${img.length})`;
+
+let slideAtual = 0;
+
+function vai(){
+    slideAtual++;
+    if(slideAtual > (img.length -1)){
+        slideAtual = 0;
+    }
+    mudarMargem(slideAtual);
+}
+
+function volta(){
+    slideAtual--;
+    if(slideAtual < 0 ){
+        slideAtual = (img.length - 1);
+    }
+    mudarMargem(slideAtual);
+}
+
+function mudarMargem(n){
+    slider.style.marginLeft = `calc(-100vw * ${n})`;
+}
+
